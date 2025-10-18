b4367e3699b1a818c27f8c2fdf506c9f540562ec
Atikur Rahman <atikur947382@gmail.com>
2025-10-18 13:28:41 +0600

    added post api to add income to database

--- server.js
+++ server.js
@@ -3,12 +3,11 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-
 
 // database connection
 const uri = process.env.MONGODB_URI;
@@ -19,16 +18,36 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const db = client.db("Expense_Tracker_DB");
+    const incomesCollection = db.collection("incomes");
+
+    // api to add income to database
+    app.post("/income", async (req, res) => {
+      try {
+        const income = req.body;
+        income.createdAt = new Date();
+
+        const result = await incomesCollection.insertOne(income);
+        res.status(201).json(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Failed to add income" });
+      }
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -36,11 +55,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Expense tracker server')
-})
+app.get("/", (req, res) => {
+  res.send("Expense tracker server");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
