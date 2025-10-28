56d459aa897cbf942daa6236c3c46a84c78823b4
Atikur Rahman <atikur947382@gmail.com>
2025-10-28 15:35:23 +0600

    project completed

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("Expense_Tracker_DB");
     const incomeCollection = db.collection("incomes");
@@ -78,7 +78,7 @@ async function run() {
             source: updatedIncome.source,
             amount: Number(updatedIncome.amount),
             date: updatedIncome.date,
-            icon: updatedIncome.emoji || updatedIncome.icon || "💰", // handle emoji/icon
+            icon: updatedIncome.emoji || updatedIncome.icon || "💰",
           },
         };
 
@@ -264,10 +264,10 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
