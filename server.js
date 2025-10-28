9ef1fce614ad60c197a2bb847d6850e6acde5a8a
Atikur Rahman <atikur947382@gmail.com>
2025-10-28 17:17:52 +0600

    made some changes in mongodb uri

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ app.use(express.json());
 app.use(cors());
 
 // database connection
-const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.g93sy5b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vrhjsg7.mongodb.net/?appName=Cluster0`;
+
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
